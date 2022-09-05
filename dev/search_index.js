3b9ad73b5c82859bfb895a9e8e74c53d9ef42ea7
Documenter.jl <documenter@juliadocs.github.io>
2022-09-05 09:19:23 +0000

    build based on 31873b7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HISOL","category":"page"},{"location":"#HISOL","page":"Home","title":"HISOL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HISOL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HISOL]","category":"page"}]
+}
