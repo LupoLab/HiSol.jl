c3c7276e619b2560d96b237b63ce6c24eb70719d
chrisbrahms <c.brahms@hw.ac.uk>
2022-09-14 11:18:23 +0100

    combined function for compressor

--- src/Compressor.jl
+++ src/Compressor.jl
@@ -27,9 +27,7 @@ function optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
 
     _, P0 = T0P0(τfwhm_in, energy)
 
-    # F**2 = 1 + 4/3sqrt3 φm**2
-    # φm = sqrt(3sqrt3/4 *(F**2 - 1))
-    φm = sqrt(3*sqrt(3)/4 * (factor^2 - 1))
+    φm = nonlinear_phase(factor)
 
     # φm = γP0Leff
     γLeff = φm/P0
@@ -51,7 +49,7 @@ function optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
                                      entrance_window, exit_window)
         if flength <= 0
             @debug(@sprintf("a = %.1f μm: required window distance is too large",1e6aguess))
-            aguess *= 0.9
+            aguess *= 0.99
             continue
         end
         γLeff_this = n2*k0/(A0*aguess^2)*Leff(flength, aguess, λ0)
@@ -89,11 +87,9 @@ function optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
     return aopt, flength, pr, t
 end
 
-function plot_optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
-                       thickness=1e-3, material=:SiO2, zr_frac=0.2, S_sf=1.5, S_ion=10,
-                       entrance_window=true, exit_window=true, LIDT=2000, S_fluence=5,
-                       amin=10e-6, amax=500e-6, Na=512)
-    factor = τfwhm_in/τfwhm_out
+function params_maxlength(τfwhm_in, gas, λ0, energy, maxlength;
+                          thickness=1e-3, material=:SiO2, zr_frac=0.2, S_sf=1.5,
+                          entrance_window=true, exit_window=true, LIDT=2000, S_fluence=5)
 
     ρcrit = critical_density(gas, λ0, τfwhm_in, energy)
     ρ = ρcrit/S_sf
@@ -102,50 +98,68 @@ function plot_optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
 
     _, P0 = T0P0(τfwhm_in, energy)
 
-    # F**2 = 1 + 4/3sqrt3 φm**2
-    # φm = sqrt(3sqrt3/4 *(F**2 - 1))
-    φm = sqrt(3*sqrt(3)/4 * (factor^2 - 1))
-
-    # φm = γP0Leff
-    γLeff = φm/P0
-
     k0 = 2π/λ0
+    A0 = Aeff0()
+
+    function params(a)
+        flength = max_flength(a, λ0, energy, τfwhm_in, maxlength;
+                              thickness, material, zr_frac, LIDT, S_fluence,
+                              entrance_window, exit_window)
+        γLeff = n2*k0/(A0*a^2)*Leff(flength, a, λ0)
+        t = transmission.(flength, a, λ0)
+        intensity = P0/(A0*a^2)
+        φnl = P0*γLeff
+        (;φnl=P0*γLeff, transmission=t, intensity, flength,
+          broadening_factor=broadening_factor(φnl), pressure=pr, n2=n2,
+          P0=P0)
+    end
+end
+
+function plot_optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
+                       thickness=1e-3, material=:SiO2, zr_frac=0.2, S_sf=1.5, S_ion=10,
+                       entrance_window=true, exit_window=true, LIDT=2000, S_fluence=5,
+                       amin=10e-6, amax=500e-6, Na=512)
+    factor = τfwhm_in/τfwhm_out
+
+    f = params_maxlength(τfwhm_in, gas, λ0, energy, maxlength;
+                         thickness, material, zr_frac, S_sf,
+                         entrance_window, exit_window, LIDT, S_fluence)
+
 
     Isupp = barrier_suppression_intensity(gas)
     A0 = Aeff0()
 
     a = collect(range(amin, amax, Na))
-    flength = map(a) do ai
-        max_flength(ai, λ0, energy, τfwhm_in, maxlength;
-                    thickness, material, zr_frac, LIDT, S_fluence,
-                    entrance_window, exit_window)
-    end
-    γLeff_a = @. n2*k0/(A0*a^2)*Leff(flength, a, λ0)
-    t = transmission.(flength, a, λ0)
-    t[flength .== 0] .= NaN
-    intensity = @. P0/(A0*a^2)
+    params = f.(a)
+    broadfac = getindex.(params, :broadening_factor)
+    flength = getindex.(params, :flength)
+    t = getindex.(params, :transmission)
+    intensity = getindex.(params, :intensity)
+    P0 = params[1].P0
+    pr = params[1].pressure
 
     try
         global aopt, flopt, _, topt = optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
                         thickness, material, zr_frac, S_sf, S_ion, LIDT, S_fluence, entrance_window, exit_window)
         global intopt = P0/(A0*aopt^2)
-    catch
+    catch e
         global aopt = missing
     end
 
     fig = plt.figure()
     fig.set_size_inches(15, 4)
     plt.subplot(1, 4, 1)
-    plt.plot(1e6a, P0*γLeff_a)
+    plt.plot(1e6a, broadfac)
     if ~ismissing(aopt)
-        plt.plot(1e6aopt, P0*γLeff, "o"; color="k", label=@sprintf("%.1f μm", 1e6aopt))
+        plt.plot(1e6aopt, factor, "o"; color="k", label=@sprintf("%.1f μm", 1e6aopt))
     end
-    plt.axhline(P0*γLeff; linestyle="--", color="k", label="Required")
+    plt.axhline(factor; linestyle="--", color="k", label="Required")
     plt.xlim(1e6.*extrema(a))
     plt.ylim(ymin=0)
     plt.legend()
     plt.xlabel("Core radius (μm)")
-    plt.ylabel("P\$_0\$ × γ × \$L_{eff}\$ (req.)")
+    plt.ylabel("Broadening factor")
+
     
     plt.subplot(1, 4, 2)
     plt.plot(1e6a, flength)
@@ -188,4 +202,8 @@ function plot_optimise(τfwhm_in, τfwhm_out, gas, λ0, energy, maxlength;
 
     return fig
 end
+
+nonlinear_phase(broadfac) = sqrt(3*sqrt(3)/4 * (broadfac^2 - 1))
+broadening_factor(φnl) = sqrt(1 + 4/3√3*φnl^2)
+
 end
