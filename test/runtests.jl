92917c698d15bb694cc3d0508f5bb8820f276af2
chrisbrahms <c.brahms@hw.ac.uk>
2022-09-27 14:10:49 +0100

    rm top-level testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,15 @@
 using HiSol
 using Test
 
-@testset "HiSol.jl" begin
-    @testset "Phase-matching" begin
-        include("test_phasematching.jl")
-    end
-    @testset "Limits" begin
-        include("test_limits.jl")
-    end
-    @testset "Design" begin
-        include("test_design.jl")
-    end
-    @testset "Solitons" begin
-        include("test_solitons.jl")
-    end
+@testset "Phase-matching" begin
+    include("test_phasematching.jl")
+end
+@testset "Limits" begin
+    include("test_limits.jl")
+end
+@testset "Design" begin
+    include("test_design.jl")
+end
+@testset "Solitons" begin
+    include("test_solitons.jl")
 end
